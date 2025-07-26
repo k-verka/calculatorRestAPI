442f60976acab631acdb8ce624da80a4d811f7a4
erop <egorkaverin@yandex.com>
2025-07-27 00:02:54 +0500

    GET запрос добавил

---
+++ server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Knetic/govaluate"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Calculation struct {
+	ID         string `json:"id"`
+	Expression string `json:"expression"`
+	Result     string `json:"result"`
+}
+
+type CalculationRequest struct {
+	Expression string `json:"expression"`
+}
+
+var calculations = []Calculation{}
+
+func calculateExpression(expression string) (string, error) {
+	expr, err := govaluate.NewEvaluableExpression(expression)
+	if err != nil {
+		return "", err
+	}
+	result, err := expr.Evaluate(nil)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%v", result), err
+}
+
+func getCalculations(c echo.Context) error {
+	return c.JSON(http.StatusOK, calculations)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/calculations", getCalculations)
+	e.Start("localhost:8080")
+}
