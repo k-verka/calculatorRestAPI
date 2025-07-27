508c9bfcf97375dcbff90aa8c6277e279e6dd50f
erop <egorkaverin@yandex.com>
2025-07-27 14:24:25 +0500

    POST запрос добавил

--- server/main.go
+++ server/main.go
@@ -1,3 +1,4 @@
+// Импорты
 package main
 
 import (
@@ -5,10 +6,12 @@ import (
 	"net/http"
 
 	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// Модели
 type Calculation struct {
 	ID         string `json:"id"`
 	Expression string `json:"expression"`
@@ -21,6 +24,7 @@ type CalculationRequest struct {
 
 var calculations = []Calculation{}
 
+// Логика вычисления
 func calculateExpression(expression string) (string, error) {
 	expr, err := govaluate.NewEvaluableExpression(expression)
 	if err != nil {
@@ -33,10 +37,34 @@ func calculateExpression(expression string) (string, error) {
 	return fmt.Sprintf("%v", result), err
 }
 
+// GET
 func getCalculations(c echo.Context) error {
 	return c.JSON(http.StatusOK, calculations)
 }
 
+// POST
+func postCalculations(c echo.Context) error {
+	req := new(CalculationRequest)
+
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	result, err := calculateExpression(req.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
+	}
+
+	calc := Calculation{
+		ID:         uuid.NewString(),
+		Expression: req.Expression,
+		Result:     result,
+	}
+	calculations = append(calculations, calc)
+	return c.JSON(http.StatusCreated, calc)
+}
+
+// Точка входа
 func main() {
 	e := echo.New()
 
@@ -44,5 +72,8 @@ func main() {
 	e.Use(middleware.Logger())
 
 	e.GET("/calculations", getCalculations)
+	e.POST("/calculations", postCalculations)
+
 	e.Start("localhost:8080")
+
 }
